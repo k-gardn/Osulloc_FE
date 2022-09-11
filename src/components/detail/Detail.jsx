c03057672aba8335047dfb8bfefcef2cdb1106b7
k-gardn <rladnjs15@naver.com>
2022-09-11 16:44:51 +0900

    Design: 상품 상세 페이지 UI 구현.

--- src/components/detail/Detail.jsx
+++ src/components/detail/Detail.jsx
@@ -87,168 +87,189 @@ const Detail = () => {
     };
 
     return (
-        <Grid>
-            {/* <div className={styles.container}>detail</div> */}
-            <div className={styles.item_info}>
-                <div className={styles.leftbox}>
-                    <div className={styles.item_layout}>
-                        <div className={styles.imgbox}>
-                            <img
-                                className={styles.img}
-                                src="https://www.osulloc.com/upload/kr/ko/adminImage/JY/QU/20210825170710069ZN.png?quality=80"
-                                alt=""
-                            />
-                            <div className={styles.buyer_benefits}>
-                                <Box sx={{ width: "100%" }}>
-                                    <Grid
-                                        container
-                                        rowSpacing={1}
-                                        columnSpacing={{ xs: 1, sm: 2, md: 3 }}
-                                    >
-                                        <Grid xs={4}>
-                                            <Item>
-                                                <span>💍</span>
-                                                뷰티포인트
-                                            </Item>
-                                        </Grid>
-                                        <Grid xs={4}>
-                                            <Item>
-                                                <span>🍀</span>
-                                                찻잎 450p 적립
-                                            </Item>
-                                        </Grid>
-                                        <Grid xs={4}>
-                                            <Item>
-                                                <span>🚛</span>
-                                                3만원 이상 무료배송
-                                            </Item>
-                                        </Grid>
-                                        <Grid xs={4}>
-                                            <Item>
-                                                <span>🎁</span>
-                                                (유료)포장가능
-                                            </Item>
-                                        </Grid>
-                                        <Grid xs={4}>
-                                            <Item>
-                                                <span>👜</span>
-                                                쇼핑백 동봉
-                                            </Item>
-                                        </Grid>
-                                    </Grid>
-                                </Box>
+        <>
+            <div className={styles.container}>
+                <div className={styles.item_info}>
+                    <div className={styles.leftbox}>
+                        <div className={styles.item_layout}>
+                            <div className={styles.imgbox}>
+                                <img
+                                    className={styles.img}
+                                    src="https://www.osulloc.com/upload/kr/ko/adminImage/JY/QU/20210825170710069ZN.png?quality=80"
+                                    alt=""
+                                />
+                                <div className={styles.buyer_benefits}>
+                                    <div className={styles.items}>
+                                        <span>💍 뷰티포인트</span>
+                                    </div>
+
+                                    <div className={styles.items}>
+                                        <span>🍀 찻잎 450p 적립</span>
+                                    </div>
+                                    <div className={styles.items}>
+                                        <span>🚛 3만원 이상 무료배송</span>
+                                    </div>
+
+                                    <div className={styles.items}>
+                                        <span>🎁 (유료)포장가능</span>
+                                    </div>
+                                    <div className={styles.items}>
+                                        <span>👜 쇼핑백 동봉</span>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-                <div className={styles.rightbox}>
-                    <div>
-                        <Stack className={styles.breadcrumbs} spacing={2}>
-                            <Breadcrumbs separator="›" aria-label="breadcrumb">
-                                {breadcrumbs}
-                            </Breadcrumbs>
-                        </Stack>
-                        <p className={styles.productName}>시크릿 티 스토리</p>
-                        <p className={styles.produtContent}>
-                            신비로운 섬 제주의 다채로운 향기와 이야기를 품은,
-                            취향 걱정없이 선물하기 좋은 고급스러운 구성의 선물
-                            세트
-                        </p>
+                    <div className={styles.rightbox}>
                         <div>
-                            <div className={styles.btnAndPriceBox}>
-                                <span className={styles.smallbtnset}>
-                                    {/* TODO: a 태그로 하는 방법 생각해보기  */}
-                                    <button className={styles.urlbtn}></button>
-                                    <button className={styles.fbtn}></button>
-                                    {/* <button className={styles.btn}>🤍</button> */}
-                                </span>
-                                <p>45000원</p>
-                            </div>
-                            <Grid>
-                                <FormControl
-                                    className={styles.addproducts}
-                                    sx={{ m: 1, minWidth: 120 }}
-                                    fullWidth
+                            <Stack className={styles.breadcrumbs} spacing={2}>
+                                <Breadcrumbs
+                                    separator="›"
+                                    aria-label="breadcrumb"
                                 >
-                                    <Select
-                                        value={age}
-                                        onChange={handleChange}
-                                        displayEmpty
-                                        inputProps={{
-                                            "aria-label": "Without label",
-                                        }}
-                                    >
-                                        <MenuItem value="">
-                                            <em>추가 상품 선택</em>
-                                        </MenuItem>
-                                        <MenuItem value={10}>
-                                            녹차 밀크 스프레드 세트
-                                            <span>20000원</span>
-                                        </MenuItem>
-                                        <MenuItem value={20}>
-                                            러블리 티 박스
-                                            <span>20000원</span>
-                                        </MenuItem>
-                                    </Select>
-                                    {/* <FormHelperText>Without label</FormHelperText> */}
-                                </FormControl>
-                            </Grid>
-
+                                    {breadcrumbs}
+                                </Breadcrumbs>
+                            </Stack>
+                            <p className={styles.productName}>
+                                시크릿 티 스토리
+                            </p>
+                            <p className={styles.produtContent}>
+                                신비로운 섬 제주의 다채로운 향기와 이야기를
+                                품은, 취향 걱정없이 선물하기 좋은 고급스러운
+                                구성의 선물 세트
+                            </p>
                             <div>
-                                <div className={styles.productbottombox}>
-                                    <div className={styles.productflexbox}>
-                                        <label>구매 수량</label>
-                                        <div className={styles.productcount}>
-                                            <button>-</button>
-                                            <span> 2 </span>
-                                            <button>+</button>
-                                        </div>
+                                <div className={styles.btnAndPriceBox}>
+                                    <div className={styles.smallbtnset}>
+                                        {/* TODO: a 태그로 하는 방법 생각해보기  */}
+                                        <button className={styles.urlbtn}>
+                                            {" "}
+                                        </button>
+                                        <button className={styles.fbtn}>
+                                            {" "}
+                                        </button>
                                     </div>
-
+                                    <p>45000원</p>
+                                </div>
+                                <Grid>
                                     <FormControl
-                                        className={styles.package}
-                                        sx={{ m: 1, minWidth: 120 }}
+                                        className={styles.addproducts}
+                                        sx={{ m: 0, minWidth: 120 }}
                                         fullWidth
+                                        margin="normal"
                                     >
                                         <Select
-                                            value={pack}
-                                            onChange={packhandleChange}
+                                            value={age}
+                                            onChange={handleChange}
                                             displayEmpty
                                             inputProps={{
                                                 "aria-label": "Without label",
                                             }}
                                         >
                                             <MenuItem value="">
-                                                <em>포장 가능(+2000원)</em>
+                                                <em>추가 상품 선택</em>
                                             </MenuItem>
-
                                             <MenuItem value={10}>
-                                                포장안함
+                                                녹차 밀크 스프레드 세트
+                                                <span>20000원</span>
                                             </MenuItem>
                                             <MenuItem value={20}>
-                                                포장함
+                                                러블리 티 박스
+                                                <span>20000원</span>
                                             </MenuItem>
                                         </Select>
+                                        {/* <FormHelperText>Without label</FormHelperText> */}
                                     </FormControl>
-                                </div>
+                                </Grid>
+
                                 <div>
-                                    <div className={styles.delivery}>
-                                        무료배송
+                                    <div className={styles.productbottombox}>
+                                        <div className={styles.productflexbox}>
+                                            <label>구매 수량</label>
+                                            <div
+                                                className={styles.productcount}
+                                            >
+                                                <button>-</button>
+                                                <span
+                                                    style={{
+                                                        fontSize: "1.5rem",
+                                                    }}
+                                                >
+                                                    2
+                                                </span>
+                                                <button>+</button>
+                                            </div>
+                                        </div>
+                                        <div className={styles.package}>
+                                            <FormControl
+                                                sx={{ m: 0, minWidth: 120 }}
+                                                fullWidth
+                                            >
+                                                <Select
+                                                    value={pack}
+                                                    onChange={packhandleChange}
+                                                    displayEmpty
+                                                    inputProps={{
+                                                        "aria-label":
+                                                            "Without label",
+                                                    }}
+                                                >
+                                                    <MenuItem value="">
+                                                        <em>
+                                                            포장 가능(+2000원)
+                                                        </em>
+                                                    </MenuItem>
+
+                                                    <MenuItem value={10}>
+                                                        포장안함
+                                                    </MenuItem>
+                                                    <MenuItem value={20}>
+                                                        포장함
+                                                    </MenuItem>
+                                                </Select>
+                                            </FormControl>
+                                        </div>
                                     </div>
                                     <div>
-                                        <span>상품금액합계</span>
-                                        <span>76,000원</span>
+                                        <div className={styles.delivery}>
+                                            <div
+                                                style={{
+                                                    width: "max-content",
+                                                    backgroundColor: "#6c801a",
+                                                    padding: "5px",
+                                                }}
+                                            >
+                                                무료배송
+                                            </div>
+                                        </div>
+
+                                        <div className={styles.price}>
+                                            <span className={styles.spanPrice}>
+                                                상품금액합계
+                                            </span>
+                                            <span className={styles.totalPrice}>
+                                                76,000원
+                                            </span>
+                                        </div>
+                                        <div className={styles.buyBtnSet}>
+                                            <button className={styles.giftBtn}>
+                                                선물하기
+                                            </button>
+                                            <button className={styles.cartBtn}>
+                                                장바구니
+                                            </button>
+                                            <button className={styles.buyBtn}>
+                                                바로구매
+                                            </button>
+                                        </div>
                                     </div>
-                                    <button>선물하기</button>
-                                    <button>장바구니</button>
-                                    <button>바로구매</button>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </Grid>
+        </>
     );
 };
 
--- src/components/main/banner/Banner.jsx
+++ src/components/main/banner/Banner.jsx
@@ -10,50 +10,57 @@ SwiperCore.use([Autoplay]);
 SwiperCore.use([Navigation, Pagination]);
 
 const Banner = () => {
-  const BANNER_IMG_1 =
-    "https://www.osulloc.com/upload/kr/ko/adminImage/EZ/IV/20220705112053996OM.png?quality=80";
-  const BANNER_IMG_2 =
-    "https://www.osulloc.com/upload/kr/ko/adminImage/KV/PJ/20220826104331702OY.png?quality=80";
-  const BANNER_IMG_3 =
-    "https://www.osulloc.com/upload/kr/ko/adminImage/QS/OH/20220902091418907OS.png?quality=80";
-  const BANNER_IMG_4 =
-    "https://www.osulloc.com/upload/kr/ko/adminImage/CF/HD/20220825164210367CA.png?quality=80";
+    const BANNER_IMG_1 =
+        "https://www.osulloc.com/upload/kr/ko/adminImage/EZ/IV/20220705112053996OM.png?quality=80";
+    const BANNER_IMG_2 =
+        "https://www.osulloc.com/upload/kr/ko/adminImage/KV/PJ/20220826104331702OY.png?quality=80";
+    const BANNER_IMG_3 =
+        "https://www.osulloc.com/upload/kr/ko/adminImage/QS/OH/20220902091418907OS.png?quality=80";
+    const BANNER_IMG_4 =
+        "https://www.osulloc.com/upload/kr/ko/adminImage/CF/HD/20220825164210367CA.png?quality=80";
 
-  const bannerImgs = [BANNER_IMG_1, BANNER_IMG_2, BANNER_IMG_3, BANNER_IMG_4];
+    const bannerImgs = [BANNER_IMG_1, BANNER_IMG_2, BANNER_IMG_3, BANNER_IMG_4];
 
-  const width = window.outerWidth;
-  console.log("width ?", width);
+    const width = window.outerWidth;
+    console.log("width ?", width);
 
-  return (
-    <section className={styles.bannerContainer}>
-      <Swiper
-        spaceBetween={0}
-        slidesPerView={1}
-        navigation
-        pagination={{ clickable: true }}
-        autoplay={{ delay: 3000 }}
-      >
-        {bannerImgs.map((img, idx) => (
-          <SwiperSlide key={idx}>
-            <div className={styles.banner}>
-              <img className={styles.bannerImg} src={img} alt="banner" />
-              <div className={styles.bannerTextBox}>
-                <p className={styles.bannerTitle}>
-                  가을에 어울리는 <br />
-                  티&티푸드 페어링
-                </p>
-                <p className={styles.bannerDescription}>
-                  오설록이 제안하는 티와 티푸드 조홥으로 <br />
-                  성큼 다가온 가을을 맞이해보세요.
-                </p>
-                <p className={styles.bannerDate}>09.02 - 09.18</p>
-              </div>
-            </div>
-          </SwiperSlide>
-        ))}
-      </Swiper>
-    </section>
-  );
+    return (
+        <section className={styles.bannerContainer}>
+            <Swiper
+                spaceBetween={0}
+                slidesPerView={1}
+                navigation
+                pagination={{ clickable: true }}
+                autoplay={{ delay: 3000 }}
+            >
+                {bannerImgs.map((img, idx) => (
+                    <SwiperSlide key={idx}>
+                        <div className={styles.banner}>
+                            <img
+                                className={styles.bannerImg}
+                                src={img}
+                                alt="banner"
+                            />
+                            <div className={styles.bannerTextBox}>
+                                <p className={styles.bannerTitle}>
+                                    가을에 어울리는 <br />
+                                    티&티푸드 페어링
+                                </p>
+                                <p className={styles.bannerDescription}>
+                                    오설록이 제안하는 티와 티푸드 조홥으로{" "}
+                                    <br />
+                                    성큼 다가온 가을을 맞이해보세요.
+                                </p>
+                                <p className={styles.bannerDate}>
+                                    09.02 - 09.18
+                                </p>
+                            </div>
+                        </div>
+                    </SwiperSlide>
+                ))}
+            </Swiper>
+        </section>
+    );
 };
 
 export default Banner;
