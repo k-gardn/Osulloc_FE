4a774b498bbb245bf3d94806d0e83a6ff316e3d9
hyein <poptarts@kakao.com>
2022-09-13 21:37:56 +0900

    Feat: kakao 로그인 정상 구현

--- src/components/login/KakaoRedirect.jsx
+++ src/components/login/KakaoRedirect.jsx
@@ -6,26 +6,19 @@ const KakaoRedirect = () => {
   const navigate = useNavigate();
   // 인가코드
   let code = new URL(window.location.href).searchParams.get("code");
-  console.log(code);
 
-  useEffect(
-    () =>
-      async function kakaologin() {
-        console.log("시작되고있는가");
-        await axios
-          .get(`http://3.36.123.198/api/oauth/kakao?code=${code}`)
-          .then((res) => {
-            console.log(res);
-            const ACCESS_TOKEN = res.data.accessToken;
-            sessionStorage.setItem("token", ACCESS_TOKEN); //예시로 로컬에 저장함
-            navigate("/"); // 토큰 받았았고 로그인됐으니 화면 전환시켜줌(메인으로)
-          })
-          .catch((err) => {
-            console.log("소셜로그인 에러", err);
-          });
-      },
-    []
-  );
+  useEffect(() => {
+    axios
+      .get(`http://3.36.123.198/api/oauth/kakao?code=${code}`)
+      .then((res) => {
+        const ACCESS_TOKEN = res.data.accessToken;
+        sessionStorage.setItem("token", ACCESS_TOKEN); //예시로 로컬에 저장함
+        navigate("/"); // 토큰 받았았고 로그인됐으니 화면 전환시켜줌(메인으로)
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 };
 
 export default KakaoRedirect;
