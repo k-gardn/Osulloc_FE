daf83c96ca751a9b58cb2dd17e380a93b7367436
k-gardn <rladnjs15@naver.com>
2022-09-13 21:11:13 +0900

    Feat: cart get 수정중

---
+++ src/components/cart_item/CartItem.jsx
@@ -0,0 +1,109 @@
+import React from "react";
+import styles from "./CartItem.module.css";
+import Button from "@mui/material/Button";
+import ButtonGroup from "@mui/material/ButtonGroup";
+import { useState } from "react";
+import { useEffect } from "react";
+import Checkbox from "@mui/material/Checkbox";
+import { useDispatch } from "react-redux";
+import {
+  cartCountChange,
+  deletecartproduct,
+} from "../../redux/modules/cartSlice";
+
+const CartItem = ({ item, onChangeProps }) => {
+  const dispatch = useDispatch();
+  const { productId, name, count, img1, price, isChecked, pack } = item;
+  //   console.log("item isChecked > ", isChecked);
+
+  const [checked, setChecked] = useState(isChecked);
+  const [isBtnValid, setIsBtnValid] = useState(true);
+  const [wrapping, setWrapping] = useState("");
+
+  useEffect(() => {
+    if (pack === true) {
+      return setWrapping("포장 부가서비스");
+    } else {
+      return setWrapping("포장 가능");
+    }
+  }, [cartCountChange]);
+
+  const handleChange = (event) => {
+    console.log("event.target.checked", event.target.checked);
+    setChecked(event.target.checked);
+    onChangeProps(productId, "isChecked", event.target.checked);
+  };
+
+  const amountInputHandler = (event) => {
+    onChangeProps(productId, "count", +event.target.value);
+    const writeCount = {
+      productId,
+      count,
+    };
+    dispatch(cartCountChange(writeCount));
+  };
+
+  const amountIncreaseHandler = (event) => {
+    event.preventDefault();
+    onChangeProps(productId, "count", count + 1);
+    const plusCount = {
+      productId,
+      count: count + 1,
+    };
+    dispatch(cartCountChange(plusCount));
+  };
+
+  const amountDecreaseHandler = (event) => {
+    event.preventDefault();
+    onChangeProps(productId, "count", count - 1);
+    const plusCount = {
+      productId,
+      count: count - 1,
+    };
+    dispatch(cartCountChange(plusCount));
+  };
+
+  const deleteHandler = (event) => {
+    event.preventDefault();
+    dispatch(deletecartproduct(productId));
+  };
+
+  //TODO: - 버튼의 활성화. 아이템 수의 변동이 있을 때를 체크.
+  useEffect(() => {
+    setIsBtnValid(count > 1);
+  }, [count]);
+
+  return (
+    <div className={styles.cartItemContainer}>
+      {/* <Checkbox
+        checked={isChecked}
+        onChange={handleChange}
+        inputProps={{ "aria-label": "controlled" }}
+      /> */}
+      <button onClick={deleteHandler}>삭제하기</button>
+      <div className={styles.container}>
+        <img src={img1} alt="productImg" className={styles.img} />
+        <div className={styles.info}>
+          <p>{name}</p>
+          <p>{wrapping}</p>
+          <p>선물 가능한 상품입니다</p>
+        </div>
+        <div className={styles.cntBox}>
+          <button onClick={amountDecreaseHandler} disabled={!isBtnValid}>
+            -
+          </button>
+          <input
+            type="text"
+            onChange={amountInputHandler}
+            value={count}
+          ></input>
+          <button onClick={amountIncreaseHandler}>+</button>
+          {price * count}
+        </div>
+        <button>바로구매</button>
+      </div>
+    </div>
+  );
+};
+
+export default CartItem;
--- src/redux/modules/detailSlice.js
+++ src/redux/modules/detailSlice.js
@@ -3,54 +3,56 @@ import { instance } from "../../network/request";
 
 //TODO: 디테일 페이지 상품 정보 get.
 export const getdetail = createAsyncThunk(
-    "GET_DETAIL_PRODUCT",
-    async (product_id, thunkAPI) => {
-        console.log("ddd");
-        try {
-            const res = await instance.get(`api/main/products/${product_id}`);
-            // const res = await instance.get(`/detail`);
-            // console.log("slice", res.data);
-            return thunkAPI.fulfillWithValue(res.data);
-        } catch (error) {
-            return thunkAPI.rejectWithValue(error);
-        }
+  "GET_DETAIL_PRODUCT",
+  async (product_id, thunkAPI) => {
+    console.log("ddd");
+    try {
+      const res = await instance.get(`api/main/products/${product_id}`);
+      // const res = await instance.get(`/detail`);
+      // console.log("slice", res.data);
+      return thunkAPI.fulfillWithValue(res.data);
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error);
     }
+  }
 );
 
 //TODO: 상품정보 post.
 export const postdetail = createAsyncThunk(
-    "POST_DETAIL_PRODUCT",
-    async (payload, thunkAPI) => {
-        try {
-            const res = await instance.get(`/api/auth/main/products/cart`);
-            // const res = await instance.post(`/cart`, payload);
-            // console.log("slice", res.data);
-            return thunkAPI.fulfillWithValue(res.data);
-        } catch (error) {
-            return thunkAPI.rejectWithValue(error);
-        }
+  "POST_DETAIL_PRODUCT",
+  async (payload, thunkAPI) => {
+    try {
+      const res = await instance.post(`/api/auth/main/products/cart`, payload);
+      // const res = await instance.post(`/cart`, payload);
+      // console.log("slice", res.data);
+      return thunkAPI.fulfillWithValue(
+        res.data.success ? res.data : res.data.error
+      );
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error);
     }
+  }
 );
 
 const detailSlice = createSlice({
-    name: "detail",
-    initialState: { detail: [] },
-    reducers: {},
-    extraReducers: {
-        //TODO: 디테일 페이지 상품 정보 get.
-        [getdetail.fulfilled]: (state, action) => {
-            state.isLoading = false;
-            state.detail = action.payload.data;
-            // console.log(action.payload.data);
-        },
-
-        //TODO: addTravelCard Thunk
-        [postdetail.fulfilled]: (state, action) => {
-            state.success = false;
-            state.cart.push(action.payload);
-            console.log(action);
-        },
+  name: "detail",
+  initialState: { detail: [] },
+  reducers: {},
+  extraReducers: {
+    //TODO: 디테일 페이지 상품 정보 get.
+    [getdetail.fulfilled]: (state, action) => {
+      state.isLoading = false;
+      state.detail = action.payload.data;
+      // console.log(action.payload.data);
     },
+
+    //TODO: postdetail post.
+    [postdetail.fulfilled]: (state, action) => {
+      state.success = false;
+      state.cart.push(action.payload);
+      console.log(action);
+    },
+  },
 });
 
 export default detailSlice.reducer;
