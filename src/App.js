25eac112c36d680dc87fa20d5e105886b7e8ac4f
hyein <poptarts@kakao.com>
2022-09-14 22:23:25 +0900

    Design: 파비콘, 헤더 설정

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import MainPage from "./pages/MainPage";
 import ProductList from "./pages/ProductListPage";
-import { CookiesProvider } from "react-cookie";
 import Login from "./components/login/Login";
 import Join from "./components/join/Join";
 import DetailPage from "./pages/DetailPage";
@@ -12,20 +11,18 @@ import KakaoRedirect from "./components/login/KakaoRedirect";
 
 function App() {
   return (
-    <CookiesProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<MainPage />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/join" element={<Join />} />
-          <Route path="/cart" element={<CartPage />} />
-          <Route path="/products" element={<ProductList />} />
-          <Route path="/products/:id" element={<DetailPage />} />
-          <Route path="/cartleeseul" element={<CartLeeseulPage />} />
-          <Route path="/auth" element={<KakaoRedirect />} />
-        </Routes>
-      </BrowserRouter>
-    </CookiesProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/join" element={<Join />} />
+        <Route path="/cart" element={<CartPage />} />
+        <Route path="/products" element={<ProductList />} />
+        <Route path="/products/:id" element={<DetailPage />} />
+        <Route path="/cartleeseul" element={<CartLeeseulPage />} />
+        <Route path="/auth" element={<KakaoRedirect />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 export default App;
